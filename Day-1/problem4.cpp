cc299cac332317d8da470c091004194d8b104bd3
Versh-03 <25113cn167@glbitm.ac.in>
2026-06-02 09:55:27 +0530

    Updated problem 4

--- Day-1/problem4.cpp
+++ Day-1/problem4.cpp
@@ -7,6 +7,10 @@ int main(){
     int num,count=0;
     cout<<"Enter number: ";
     cin>>num;
+    if(num==0){
+        cout<<"The number of digits: "<<1<<endl;
+        return 0;
+    }
     while(num!=0){
         num=num/10;
         count++;
